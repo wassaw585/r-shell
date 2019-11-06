2197c99a738762aa6d571f1e0f9b5a4d7d12f2ff
Tobin Magle <tmagle@GLS-MAGR9201.lan1>
2019-11-06 12:19:57 -0600

    tried writing R loop, getting weird results on the second file

--- render-rmd.R
+++ render-rmd.R
@@ -1,12 +1,18 @@
 library(rmarkdown)
 
-args <- commandArgs(trailingOnly = TRUE)
+args <- unlist(commandArgs(trailingOnly = TRUE))
 
+length(args)
 
 #same as hitting knit, uses the default parameters
 #render("code/surveys-report.Rmd")
 
-render(input = "surveys-report.Rmd",     # file to render
-       output_dir = "reports",                #select output directory
-       params = list(file=args)   #sets the file parameter to plot1
-      )
+
+for(arg in args)
+{
+  arg
+  render(input = "surveys-report.Rmd",     # file to render
+         output_dir = "reports",                #select output directory
+         params = list(file=args)   #sets the file parameter to plot1
+        )
+}

86af09cf5f67d3413171a9d9db8e9c939b7a8804
Tobin Magle <tmagle@GLS-MAGR9201.lan1>
2019-11-06 14:44:59 -0600

    added the -a flag

--- render-rmd.R
+++ render-rmd.R
@@ -6,6 +6,15 @@ files <- commandArgs(trailingOnly = TRUE)
 #same as hitting knit, uses the default parameters
 #render("code/surveys-report.Rmd")
 
+if("-a" %in% files){
+  
+  files<-Sys.glob("data")
+  
+} else{
+    
+}
+
+
 for(file in files)
 {
   out_name<-str_split_fixed(string=file,
@@ -17,6 +26,6 @@ for(file in files)
   render(input = "surveys-report.Rmd",     # file to render
          output_file = out_name,
          output_dir = "reports",                #select output directory
-         params = list(file=file)   #sets the file parameter to plot1
+         params = list(file=file)   #sets the file name
         )
 }

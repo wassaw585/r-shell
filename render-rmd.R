7dc18366aa80a41a636324c58e81207e75a3f7bf
Steven Warren <swarren585@gmail.com>
2019-11-22 11:32:21 -0600

    Added error message

--- render-rmd.R
+++ render-rmd.R
@@ -5,7 +5,12 @@ file_names <- commandArgs(trailingOnly = TRUE)
 
 if("-a" %in% file_names){
   folder <- file_names[2]
-  file_names <- Sys.glob(str_c(folder, "/*.csv"))}
+  file_names <- Sys.glob(str_c(folder, "/*.csv"))
+  
+  if(length(file_names) == 0){
+    print("No files found in the specified forlder.")
+  }
+} else{file_names <- file_names}
 
 for(file in file_names)
 {

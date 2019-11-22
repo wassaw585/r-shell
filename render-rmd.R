b86e9bffbdff8e5abac8e1325d4abbcce41024d5
Steven Warren <swarren585@gmail.com>
2019-11-22 11:02:09 -0600

    Got input from the shell

--- render-rmd.R
+++ render-rmd.R
@@ -1,4 +1,6 @@
 library("rmarkdown")
 
+file_names <- commandArgs(trailingOnly = TRUE)
+
 render(input = "surveys-report.Rmd", output_dir = "reports",
-       params = list(file = "data/plot1.csv"))
+       params = list(file = file_names))

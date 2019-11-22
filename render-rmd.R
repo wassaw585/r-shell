f5ca4c870532439b3c10c1887d8871db929e2add
Steven Warren <swarren585@gmail.com>
2019-11-22 11:16:14 -0600

    Looping to create new file names

--- render-rmd.R
+++ render-rmd.R
@@ -1,6 +1,16 @@
 library("rmarkdown")
+library("stringr")
 
 file_names <- commandArgs(trailingOnly = TRUE)
 
-render(input = "surveys-report.Rmd", output_dir = "reports",
-       params = list(file = file_names))
+for(file in file_names)
+{
+  out_name <- str_split_fixed(string = file,
+                              pattern = "[/|.]",
+                              n = 3)
+  out_name <- str_c(out_name[2], '.html')
+  render(input = "surveys-report.Rmd",
+         output_file = out_name,
+         output_dir = "reports",
+         params = list(file = file))
+  }
